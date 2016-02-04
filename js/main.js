26e3c2e8f58436cfb337d1b53ee01565443b9e9a
stinaskold <Stina@Stinas-iMac.lan>
2016-02-04 11:00:34 +0100

    Changed to Flickr api

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-SC.initialize({
-  client_id: '964e1e7cc3b6ed40a9555ce957eec180'
-});
 
 $(document).ready(function() {
-    SC.get('/tracks/13158665', function(track) {
-        console.log(track);
-    })
+  $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=b9710296a6cb141241e13573ccd47bc2&tags=yellow,landscape,summer&tag_mode=all&sort=interestingness-desc&jsoncallback=?', function(data) {
+    console.log(data.photos.photo);
+    var html = '<ul>';
+    $.each(data.photos.photo,function(i,photo) {
+      html += '<li>';
+      // html += '<a href="' + photo.link + '" class="image">';
+      html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
+    });
+    html += '</ul>';
+    $('#photos').html(html);
+  });
 });
