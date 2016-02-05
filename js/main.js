e795578b51b75a97a6257a0c608bf85c77212ae9
stinaskold <Stina@Stinas-iMac.lan>
2016-02-05 16:48:27 +0100

    Structured code with functions

--- js/main.js
+++ js/main.js
@@ -1,94 +1,82 @@
+// Created by Stina Sköld
+
+// variabel to store apikey
 var apikey = 'b9710296a6cb141241e13573ccd47bc2';
+// variabel to store url for AJAX request
+var url;
+// variabel to store which Flickr API method is to be used
+var method;
+
+// Use data to display photos
+function displayPhotos(data) {
+  var html = '<ul>';
+  $.each(data.photos.photo,function(i,photo) {
+    photoURL = 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret;
+    html += '<li>';
+    html += '<a href="' + photoURL + '_b.jpg">';
+    html += '<img src="' + photoURL + '_n.jpg"></a></li>';
+  });
+  html += '</ul>';
+  $('#photos').html(html);
+}
+
+// Append praragraph with info about search
+function displayInfo(div) {
+  //Remove old search info paragraph
+  $(".info").remove();
+  // Append new search info paragraph
+  $(div).append('<p class="info">Processing search...</p>');
+}
 
 
-
+// On click, make request for photos with keywords provided by user
 $("#keyword-btn").click(function() {
-    $(".info").remove();
-    $(".keyword").append('<p class="info">Processing search...</p>');
+    $('#photos').empty();
+    // Append info to parent element
+    displayInfo($(this).parent());
+    // Store input from user in variables latitude and longitude
     var keywords = $("#keyword-search").val();
-    console.log(keywords);
 
-    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=b9710296a6cb141241e13573ccd47bc2&tags=' + keywords + '&tag_mode=all&sort=interestingness-desc&jsoncallback=?';
-    console.log(url);
+    method = 'flickr.photos.search';
+    url = 'https://api.flickr.com/services/rest/?method=' + method + '&format=json&api_key=' + apikey + '&tags=' + keywords + '&tag_mode=all&sort=interestingness-desc&jsoncallback=?';
 
     $.getJSON(url, function(data) {
-     console.log(data.photos.photo);
-     var html = '<ul>';
-     $.each(data.photos.photo,function(i,photo) {
-       html += '<li>';
-       // html += '<a href="' + photo.link + '" class="image">';
-       html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
-     });
-     html += '</ul>';
-     $('#photos').html(html);
      $(".info").text('Your search is completed.');
+     displayPhotos(data);
    });
 
 });
 
-
-
+// On click, make request for photos with location provided by user
 $("#location-btn").click(function() {
-    $(".info").remove();
-    $(".location").append('<p class="info">Processing search...</p>');
+    $('#photos').empty();
+    // Append info to parent element
+    displayInfo($(this).parent());
+    // Store input from user in variables latitude and longitude
     var latitude = $("#latitude").val();
     var longitude = $("#longitude").val();
-    console.log(latitude + " " + longitude);
 
-    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=' + apikey + '&lat=' + latitude + '&lon=' + longitude + '&sort=interestingness-desc&jsoncallback=?';
-    console.log(url);
-
-  //   $.ajax({
-  //     url: url,
-  //     error: function() {
-  //        $('#info').html('<p>An error has occurred</p>');
-  //     },
-  //     dataType: 'jsonp',
-  //     success: function(data) {
-  //       var html = '<ul>';
-  //         $.each(data.photos.photo,function(i,photo) {
-  //           html += '<li>';
-  //           // html += '<a href="' + photo.link + '" class="image">';
-  //           html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
-  //         });
-  //         html += '</ul>';
-  //         $('#photos').html(html);
-  //         $(".info").text('Your search is completed.');
-  //     },
-  //     type: 'GET'
-  //  });
+    method = 'flickr.photos.search';
+    url = 'https://api.flickr.com/services/rest/?method=' + method + '&format=json&api_key=' + apikey + '&lat=' + latitude + '&lon=' + longitude + '&sort=interestingness-desc&jsoncallback=?';
 
     $.getJSON(url, function(data) {
-      var html = '<ul>';
-      $.each(data.photos.photo,function(i,photo) {
-        html += '<li>';
-        // html += '<a href="' + photo.link + '" class="image">';
-        html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
-      });
-      html += '</ul>';
-      $('#photos').html(html);
       $(".info").text('Your search is completed.');
+      displayPhotos(data);
     });
 
 });
 
+// On click, make request for interesting photos
 $("#interesting-btn").click(function() {
-    $(".info").remove();
-    $(".interesting").append('<p class="info">Processing search...</p>');
+    $('#photos').empty();
+    // Append info to parent element
+    displayInfo($(this).parent());
 
-    var url = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=b9710296a6cb141241e13573ccd47bc2&sort=interestingness-desc&jsoncallback=?';
-    console.log(url);
+    method = 'flickr.interestingness.getList';
+    url = 'https://api.flickr.com/services/rest/?method=' + method + '&format=json&api_key=' + apikey +'&sort=interestingness-desc&jsoncallback=?';
 
     $.getJSON(url, function(data) {
-      var html = '<ul>';
-      $.each(data.photos.photo,function(i,photo) {
-        html += '<li>';
-        // html += '<a href="' + photo.link + '" class="image">';
-        html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
-      });
-      html += '</ul>';
-      $('#photos').html(html);
       $(".info").text('Your search is completed.');
+      displayPhotos(data);
     });
-
 });
