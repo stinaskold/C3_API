dbe259202a8c28aadfae74b654b09b482e7334e1
stinaskold <Stina@Stinas-iMac.lan>
2016-02-05 09:17:44 +0100

    Added search by location and interestingness

--- js/main.js
+++ js/main.js
@@ -1,14 +1,64 @@
+$("#keyword-btn").click(function() {
+    // $("info").html('<p>Processing search...</p>');
+    var keywords = $("#keyword-search").val();
+    console.log(keywords);
 
-$(document).ready(function() {
-  $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=b9710296a6cb141241e13573ccd47bc2&tags=yellow,landscape,summer&tag_mode=all&sort=interestingness-desc&jsoncallback=?', function(data) {
-    console.log(data.photos.photo);
-    var html = '<ul>';
-    $.each(data.photos.photo,function(i,photo) {
-      html += '<li>';
-      // html += '<a href="' + photo.link + '" class="image">';
-      html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
+    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=b9710296a6cb141241e13573ccd47bc2&tags=' + keywords + '&tag_mode=all&sort=interestingness-desc&jsoncallback=?';
+    console.log(url);
+
+    $.getJSON(url, function(data) {
+      console.log(data.photos.photo);
+      var html = '<ul>';
+      $.each(data.photos.photo,function(i,photo) {
+        html += '<li>';
+        // html += '<a href="' + photo.link + '" class="image">';
+        html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
+      });
+      html += '</ul>';
+      $('#photos').html(html);
     });
-    html += '</ul>';
-    $('#photos').html(html);
-  });
+
+});
+
+
+$("#location-btn").click(function() {
+
+    var latitude = $("#latitude").val();
+    var longitude = $("#longitude").val();
+    console.log(latitude + " " + longitude);
+
+    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=b9710296a6cb141241e13573ccd47bc2&lat=' + latitude + '&lon=' + longitude + '&sort=interestingness-desc&jsoncallback=?';
+    console.log(url);
+
+    $.getJSON(url, function(data) {
+      var html = '<ul>';
+      $.each(data.photos.photo,function(i,photo) {
+        html += '<li>';
+        // html += '<a href="' + photo.link + '" class="image">';
+        html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
+      });
+      html += '</ul>';
+      $('#photos').html(html);
+    });
+
+});
+
+$("#interesting-btn").click(function() {
+
+
+
+    var url = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=b9710296a6cb141241e13573ccd47bc2&sort=interestingness-desc&jsoncallback=?';
+    console.log(url);
+
+    $.getJSON(url, function(data) {
+      var html = '<ul>';
+      $.each(data.photos.photo,function(i,photo) {
+        html += '<li>';
+        // html += '<a href="' + photo.link + '" class="image">';
+        html += '<img src="' + 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server +'/' + photo.id + '_' + photo.secret + '_n.jpg"></a></li>';
+      });
+      html += '</ul>';
+      $('#photos').html(html);
+    });
+
 });
